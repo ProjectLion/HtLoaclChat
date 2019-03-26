a423e5a67616dbcd7b1b53dcfcc64e49c87bd3d1
Ht <huangtao@eschervr.com>
2019-03-26 14:40:38 +0800

    init project

---
+++ HtLocalChatClient/HtLocalChatClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HtLocalChatClient
+//
+//  Created by Ht on 2019/3/26.
+//  Copyright © 2019 Ht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
