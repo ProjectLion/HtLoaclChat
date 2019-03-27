0688fb8f1477155934ab1211b700a71e7c5959bd
Ht <huangtao@eschervr.com>
2019-03-27 16:38:25 +0800

    添加客户端的pods文件

---
+++ HtLocalChatClient/Pods/Target Support Files/Pods-HtLocalChatClient/Pods-HtLocalChatClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HtLocalChatClient : NSObject
+@end
+@implementation PodsDummy_Pods_HtLocalChatClient
+@end
---
+++ HtLocalChatClient/Pods/Target Support Files/Pods-HtLocalChatClientUITests/Pods-HtLocalChatClientUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HtLocalChatClientUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HtLocalChatClientUITests
+@end
